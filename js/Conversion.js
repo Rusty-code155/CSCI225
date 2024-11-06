9fd92321da51f92c93f1a4a3d4f5e067922f9355
Rusty-code155 <PEEPLET@usca.edu>
2024-11-06 17:29:40 -0500

    check

---
+++ js/Conversion.js
@@ -0,0 +1,44 @@
+// get the element 
+let spanE1 = document.getElementById('Input_type');
+let spanE2 = document.getElementById('Output_type');
+let Input = document.getElementById('Input');
+let Output= document.getElementById('Output');
+console.log("spanE1 -"+spanE1);
+console.log("spanE2 -"+spanE2 );
+// element --- property
+var n1= parseFloat( Input.value);
+var n2= parseFloat( Output.value);
+var Inputindex = spanE1.selectedIndex
+var Outputindex = spanE2.selectedIndex
+console.log(typeof n1);
+console.log(typeof n2);
+
+// Input index 0= Foot, 1= Meter, 2= Inch
+
+
+
+//process
+
+switch(Inputindex && Outputindex)
+{
+    case (Inputindex===Outputindex): n2=n1
+    break;
+    case (Inputindex===0 && Outputindex===1): n2=n1*0.3048
+    break;
+    case (Inputindex===0 && Outputindex===1): n2=n1*12
+    break;
+    case (Inputindex===1 && Outputindex===0): n2=n1*3.2808399
+    break;
+    case (Inputindex===1 && Outputindex===2): n2=n1*39.3700787
+    break;
+    case (Inputindex===2 && Outputindex===0): n2=n1/12
+    break;
+    case (Inputindex===2 && Outputindex===1): n2=n1*0.0254
+    break;
+    default: n2=n1
+    break;
+
+
+}
+//display
+document.write(n2);
