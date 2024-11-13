6d95ba47cf69bcafacbb6df0ac6cde1d75fab23b
Rusty-code155 <PEEPLET@usca.edu>
2024-11-13 14:44:10 -0500

    check

--- js/Conversion.js
+++ js/Conversion.js
@@ -1,15 +1,16 @@
+function conv(){
 // get the element 
 let spanE1 = document.getElementById('Input_type');
 let spanE2 = document.getElementById('Output_type');
 let Input = document.getElementById('Input');
-let Output= 0;
+let Output =0;
 console.log("spanE1 -"+spanE1);
 console.log("spanE2 -"+spanE2 );
 console.log("Input -" + Input);
 console.log("Output -" + Output);
 // element --- property
 var n1= parseFloat( Input.value);
-var n2= parseFloat( Output.value);
+var n2= parseFloat(Output.value);
 var Inputindex = spanE1.selectedIndex
 var Outputindex = spanE2.selectedIndex
 console.log(typeof n1);
@@ -25,22 +26,27 @@ switch(Inputindex && Outputindex)
 {
     case (Inputindex===Outputindex): var n2=n1
     break;
-    case (Inputindex===0 && Outputindex===1): var n2=n1*0.3048
+    case (Inputindex===0 && Outputindex===1): var n2=(n1*0.3048)
     break;
-    case (Inputindex===0 && Outputindex===1): var n2=n1*12
+    case (Inputindex===0 && Outputindex===1): var n2=(n1*12)
     break;
-    case (Inputindex===1 && Outputindex===0): var n2=n1*3.2808399
+    case (Inputindex===1 && Outputindex===0): var n2=(n1*3.2808399)
     break;
-    case (Inputindex===1 && Outputindex===2): var n2=n1*39.3700787
+    case (Inputindex===1 && Outputindex===2): var n2=(n1*39.3700787)
     break;
-    case (Inputindex===2 && Outputindex===0): var n2=n1/12
+    case (Inputindex===2 && Outputindex===0): var n2=(n1/12)
     break;
-    case (Inputindex===2 && Outputindex===1): var n2=n1*0.0254
+    case (Inputindex===2 && Outputindex===1): var n2=(n1*0.0254)
     break;
     default: var n2=n1
     break;
 
 
 }
+Output=n2
 //display
-document.write(n2);
+document.getElementById('output').textContent=Output.toString();}
+document.write();
+const InputE = document.getElementById("Input");
+const InputtypeE=document.getElementById("Input_type");
+InputE.addEventListener('change',conv);
